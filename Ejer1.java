f9d97910c47a7931f0d0a72efa88f782414ab22b
DanielYeray5 <136346612+DanielYeray5@users.noreply.github.com>
2024-09-02 12:36:56 -0600

    V3

--- Ejer1.java
+++ Ejer1.java
@@ -1,6 +1,21 @@
-public class Ejer1{
+import javax.swing.JOptionPane;
+
+public class Ejer1 {
     public static void main(String[] args) {
-        Sting a = "Hola";
-        
+        int sueldo;
+        int aumento;
+        String trabajador;
+
+        trabajador = JOptionPane.showInputDialog(null, "Ingresa nombre del trabajador");
+        sueldo = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa el sueldo"));
+
+        if (sueldo <= 5000) {
+            aumento = sueldo * 0.10;
+            sueldo = +aumento;
+        } else if (sueldo >= 10000) {
+            aumento = sueldo * 0.50;
+            sueldo = +aumento;
+        }
+        System.out.println("Trabajador: ");
     }
 }
